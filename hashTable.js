e3ca726925560f671c3359f3392d3bda37a7a00a
Nima sherpa <Toplouie78@gmail.com>
2022-04-14 10:50:29 -0400

    makeUnique

---
+++ hashTable.js
@@ -0,0 +1,37 @@
+const animals = [
+	"otter",
+	"dog",
+	"dog",
+	"dog",
+	"parakeet",
+	"woodchuck",
+	"cat",
+	"tardigrade",
+	"dog",
+	"cat",
+];
+
+// function makeUnique(arr) {
+// 	let obj = {};
+// 	for (let a of arr) {
+// 		obj[a] = true;
+// 	}
+// 	console.log(Object.keys(obj));
+// }
+
+function makeUnique(arr) {
+	let newArr = [];
+	let obj = {};
+	for (let a of arr) {
+		if (!obj[a]) {
+			newArr.push(a);
+		}
+		obj[a] = true;
+	}
+	console.log(obj);
+	console.log(newArr);
+	return newArr;
+}
+
+let uniqueArr = makeUnique(animals);
+console.log(uniqueArr);
